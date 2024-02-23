fd629374e381053bf0c8718c5c699d2c7f9f84ce
vikash kumar singh <drv74279@gmail.com>
2024-02-23 14:11:59 +0530

    Add navbar

--- src/App.js
+++ src/App.js
@@ -3,15 +3,39 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a className="navbar-brand" href="/">Vikashtech</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item active">
+        <a className="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="/">About</a>
+      </li>
+     
       
-        <h1> my first react app </h1>
-        <h2>my name is vikash</h2>
-        <h3>i am from siwan</h3>
-        <h4>hello</h4>
-        </header>
-    </div>
+    </ul>
+    <form className="form-inline my-2 my-lg-0">
+      <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+     <div>
+      <center>
+        <h1>Welcome to vikashtech</h1>
+      </center>
+     </div>
+    
+    </>
+      
+    
    
   );
 }
