e880ab09a9ec050790179d92f3cb05bdc62cb65e
vikash kumar singh <drv74279@gmail.com>
2024-02-26 12:32:47 +0530

    Enable Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+
 
 function App() {
   return (
     <>
      <Navbar title="Vikashtech"/>
      <div className='container'>
-     <TextForm heading="Enter the text to analyze"/>
+     <About></About>
       
      </div>
     </>
